9eb34156b392a53322967a6c8d5c0b4f01faaa19
Alisson Oliveira <alissonoliveiram@gmail.com>
2023-06-18 23:06:03 -0300

    feat: socket client added

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-from infrastucture.ia.calc import abc
+from infrastructure.ia.calc import abc
 
 app = Flask(__name__)
 cors = CORS(app)
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,47 +1,75 @@
-import { useEffect, useState } from "react"
-import { BsHouse } from"react-icons/bs"
-import {
-  Link,
-} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BsHouse } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import Router from "./Routes";
+import io from "socket.io-client";
 
-const API_URL = `"http://127.0.0.1:5000"`
-
-// TODO: @Ali que nem vc tinha pedido ontem, cria duas telas 1 sendo o silo como um todo e outra sendo todos os silos separados, e um link dessa tela do silo que vai pra a tela de todos os silos, puxar os arquivos de um json, acho que eu me lembre so isso de alteração
+const API_URL = "http://127.0.0.1:5000";
+const SOCKET_URL = "http://localhost:1024";
 
 function App() {
-  const [message, setMessage] = useState("API not Working!")
-  const [temperature] = useState(32)
+  const [message, setMessage] = useState("API not Working!");
+  const [temperature] = useState(32);
+  const [socket, setSocket] = useState(null);
 
   const handleHealthCheck = async () => {
-    const data = await (await fetch(API_URL, {method: "GET"})).json();
+    const data = await (await fetch(API_URL, { method: "GET" })).json();
 
     setTimeout(() => {
       setMessage(data.message);
     }, 3000);
-  }
+  };
 
   useEffect(() => {
     handleHealthCheck();
-  }, [])
+  }, []);
 
   useEffect(() => {
     console.log(message);
-  }, [message])
+  }, [message]);
+
+  useEffect(() => {
+    const socket = io(SOCKET_URL);
+    setSocket(socket);
+
+    socket.on("connect", () => {
+      console.log("Connected to socket server");
+      socket.emit("message", "Ok computer");
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <div className="App mh-100 mw-100">
       <div className="w-100 p-2 justify-content-between d-flex bg-dark">
         <div className="d-flex">
-          <Link to="/" className="button rounded-1 d-flex justify-content-center align-items-center bg-info p-3 fw-bold text-white mx-1"><BsHouse /></Link>
-          <Link to="/por-silo" className="button text-decoration-none fw-bold rounded-1 d-flex justify-content-center align-items-center bg-info p-3 text-white mx-1">Visualização por silo</Link>
-          <Link to="/relatorios" className="button text-decoration-none fw-bold rounded-1 d-flex justify-content-center align-items-center bg-info p-3 text-white mx-1">Relatórios</Link>
+          <Link
+            to="/"
+            className="button rounded-1 d-flex justify-content-center align-items-center bg-info p-3 fw-bold text-white mx-1"
+          >
+            <BsHouse />
+          </Link>
+          <Link
+            to="/por-silo"
+            className="button text-decoration-none fw-bold rounded-1 d-flex justify-content-center align-items-center bg-info p-3 text-white mx-1"
+          >
+            Visualização por silo
+          </Link>
+          <Link
+            to="/relatorios"
+            className="button text-decoration-none fw-bold rounded-1 d-flex justify-content-center align-items-center bg-info p-3 text-white mx-1"
+          >
+            Relatórios
+          </Link>
         </div>
         <div className="text-white fs-2 align-items-center">{temperature}ºC</div>
       </div>
       <Router />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
